6b395e888822a11e4628487a8121801e7686061d
Sam Kalum <stkalum@gmail.com>
2017-10-13 01:48:42 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,4 +3,13 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left column
+  [1,4,7], # Middle column
+  [2,5,8], # Right column
+  [0,4,8], # Down diagonal
+  [2,4,6]  # Up diagonal
+]
